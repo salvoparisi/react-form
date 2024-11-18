b199b281a8cb0a47124f2c8932edeccb7055d6fa
salvoparisi <psalvo065@gmail.com>
2024-11-18 14:23:36 +0100

    add setList and setTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [list, setList] = useState(["titolo 1", "titolo 2", "titolo 3", "titolo 4"]);
+  const [title, setTitle] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      setList([...list, title]);
+    }
+  };
+
+  const handleDelete = (indexToDelete) => {
+    setList(list.filter((title, index) => index !== indexToDelete));
+  };
 
   return (
     <>
-
+      <div className="container">
+        <h1>To Do List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button type="submit" className='m-3 btn btn-primary'>invia</button>
+        </form>
+        <div>
+          <ul className="list-group">
+            {list.map((title, index) => (
+              <li
+                key={index}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {title}
+                <button
+                  className="badge bg-primary rounded-pill"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
